3d376c3f64c03646198fe9e9a22df507206292c9
Yves Bode <ynrbode@gmail.com>
2018-05-14 12:29:49 -0300

    Changes to be committed: 	modified:   controllers/static.js 		Codificado as rotas '/', '/vendas', '/alugueis' 	modified:   tests/controllers/staticSpec.js 	new file:   tests/ui/alugueisSpec.txt 	new file:   tests/ui/indexSpec.txt 	new file:   tests/ui/vendasSpec.txt 		Formalizado os specs das views acima

---
+++ tests/controllers/staticSpec.js
@@ -0,0 +1,7 @@
+/*
+    Esse arquivo deve renderizar todas as páginas estáticas do sistema.
+    Implementa as rotas:
+        '/': renderiza o arquivo views/index.ejs
+        '/vendas': renderiza o arquivo views/vendas.ejs
+        '/alugueis': renderiza o arquivo views/alugueis.ejs
+*/

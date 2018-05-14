32a18e487e9521e38ee693c28ccd70fcbe5c5cf6
Yves Bode <ynrbode@gmail.com>
2018-05-14 16:12:24 -0300

    Changes to be committed: 	modified:   controllers/search.js 		Codificado todas as rotas de busca documentadas até o momento 	modified:   package.json 	modified:   tests/models/dbSpec.js 		Documentado arquivo models/db.js 	modified:   tests/models/imovelSpec.js 		Documentado arquivo models/imovel.js

--- controllers/search.js
+++ controllers/search.js
@@ -1,4 +1,4 @@
-const imovel = require('../models/imovel')
+const Imovel = require('../models/imovel')
 
 module.exports = (app) => {
     app.get('/search/vendas/:proc&:tipo&:bairro&:valor', (req, res, next) => {
@@ -21,19 +21,40 @@ module.exports = (app) => {
     })
 
     app.get('/search/vendas/destaques', (req, res, next) => {
+        var query = {}
+        query["isDestaque"] = true
 
+        Imovel.find(query, (err, imovels) => {
+            if(err) next(err)
+            else if(!imovels.length) res.send(null)
+            else res.send(imovels)
+        })
     })
 
     app.get('/search/vendas/oportunidades', (req, res, next) => {
+        var query = {}
+        query["isOportunidade"] = true
 
+        Imovel.find(query, (err, imovels) => {
+            if(err) next(err)
+            else if(!imovels.length) res.send(null)
+            else res.send(imovels)
+        })
     })
 
     app.get('/search/alugueis/:tipo&:bairro&:valor', (req, res, next) => {
+        var query = {}
+        query["isVenda"] = false
+        var reqProc   = req.params.proc
+        if(reqProc !== 'null') query["procedencia"] = reqProc
         var reqTipo   = req.params.tipo
+        if(reqTipo !== 'null') query["tipo"] = reqTipo
         var reqBairro = req.params.bairro
+        if(reqBairro !== 'null') query["bairro"] = reqBairro
         var reqValor  = req.params.valor
+        if(reqValor !== 'null') query["valor"] = {$lt : reqValor}
 
-        Imovel.find({tipo : reqTipo, bairro : reqBairro, valor : {$lt : reqValor}, isVenda: false}, (err, imovels) => {
+        Imovel.find(query, (err, imovels) => {
             if(err) next(err)
             else res.send(imovels)
         })        

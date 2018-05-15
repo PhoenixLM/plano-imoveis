3aaaeb8a0d07df55aae1ef6c6c6acfc847d6e505
Yves Bode <ynrbode@gmail.com>
2018-05-15 02:46:09 -0300

    Changes to be committed: 	modified:   .gitignore 	modified:   config/default.json 	modified:   custom-express.js 	modified:   package-lock.json 	modified:   package.json 	new file:   tests/custom-expressSpec.js 		Codificado o sistema de sessão em cookies

--- custom-express.js
+++ custom-express.js
@@ -1,10 +1,17 @@
-const express = require('express')
-const load    = require('express-load')
-
+const express       = require('express')
+const load          = require('express-load')
+const sessions      = require('client-sessions')
+const config        = require('config')
 
 module.exports = () => {
     const app = express()
-    
+
+    app.use(sessions({
+        cookieName: 'sessao',
+        secret: config.get('Session.secret'),
+        duration: config.get('Session.duration') * 60 * 60 * 1000,
+        activeDuration: 5 * 60 * 1000
+    }))
     app.use(express.static('./public'))
     app.set('view engine', 'ejs')
     load('controllers').into(app)
---
+++ tests/custom-expressSpec.js
@@ -0,0 +1,22 @@
+/*
+    Arquivo: custom-express.js
+    Tipo: JavaScript
+    Descrição:
+        Este arquivo deve definir os middlewares a serem utilizados pelo servidor, são eles:
+            Express: Framework node express
+            Express-load: Autoloader de scripts e modelos, carrega todas as rotas contidas dentro
+                da pasta controllers
+            Sessions: Define um cookie criptografado para armazenamento de dados referentes a sessão
+                do usuário, o arquivo deve definir um cookie de nome "sessao" com as configurações
+                do arquivo de configuração segundo tal:
+                    "Session": {
+                        "secret": <String>,
+                        "duration": int
+                    }
+                O atributo segredo deve ser gerado aleatóriamente e de tamanho suficiente para ser
+                seguro em ambiente de produção, a duração do cookie é definida em horas.
+
+        O arquivo deve também configurar a engine de rederização padrão para "ejs"
+
+
+ */

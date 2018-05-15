924df3ddd2f4ff1f983c94992713f6caa4eea4a9
Yves Bode <ynrbode@gmail.com>
2018-05-15 12:15:59 -0300

    Changes to be committed: 	modified:   custom-express.js 		Modificado para implemetar o middleware 'auth' 		em todas as rotas /admin/ 	modified:   middleware/auth.js 		Implementado o middleware auth segundo documentação 	modified:   tests/middleware/authSpec.js 		Implementada documentação

--- custom-express.js
+++ custom-express.js
@@ -2,6 +2,7 @@ const express       = require('express')
 const load          = require('express-load')
 const sessions      = require('client-sessions')
 const config        = require('config')
+const auth          = require('./middleware/auth')
 
 const _SECRET   = config.get('Session.secret')
 const _DURATION = config.get('Session.duration')
@@ -15,6 +16,7 @@ module.exports = () => {
         duration: _DURATION * 60 * 60 * 1000,
         activeDuration: 5 * 60 * 1000
     }))
+    app.use('/admin/*', auth)
     app.use(express.static('./public'))
     app.set('view engine', 'ejs')
     load('controllers').into(app)
